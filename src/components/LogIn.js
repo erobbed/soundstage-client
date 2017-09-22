6795955afe1bbc154280653795aed837c7470e24
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-09-21 21:49:01 -0400

    added button styles to login button

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -11,7 +11,7 @@ const LogIn = (props) => {
   return (
     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexWrap: 'wrap'}}>
       <div style={{width: '300px'}}>
-        <img width="200" src="Spotify_logo_with_text.svg.png" alt="Spotify"/>
+        <img width="200" src="Spotify_logo_with_text.svg.png" alt="Spotify" style={{paddingBottom: '5px'}}/>
         <Button className="login" as="a" href="http://localhost:3000/api/v1/login">Log In With Spotify</Button>
       </div>
     </div>
