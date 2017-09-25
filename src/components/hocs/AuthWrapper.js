84edcd050cc3e437becc56598a2a84f03d5bd892
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-09-25 17:05:38 -0400

    added loader

--- src/components/hocs/AuthWrapper.js
+++ src/components/hocs/AuthWrapper.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import NavBar from '../NavBar';
+import { Dimmer, Loader } from 'semantic-ui-react'
 
 export default function isAuthenticated(WrappedComponent){
   return function (props) {
@@ -8,12 +9,17 @@ export default function isAuthenticated(WrappedComponent){
       console.log("Redirecting back to login because I don't have a token...");
       return <Redirect to='/login' />
     }
-    
+
     return (
       <div>
         <NavBar className="nav" history={props.history}/>
+        {props.loading ? <Dimmer active>
+          <Loader />
+        </Dimmer> : null }
         <WrappedComponent {...props} />
       </div>
     )
   }
 }
+
+// {props.loading ? <img src="/Radio.svg"/> : null }
