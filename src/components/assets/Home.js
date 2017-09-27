43dcf3cc90abe9b4cacfed119a789f27cdc31b87
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-09-27 10:08:18 -0400

    stylized homepage

--- src/components/assets/Home.js
+++ src/components/assets/Home.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const Home = (props) => {
-  
+
   return (
     <div>
       <h1 id="title">SoundStage</h1>
+      <div>
+        <img src="/show.jpg" alt="home" id="home"/>
+      </div>
     </div>
   )
 }
